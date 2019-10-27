3ec3ef59afb6c8879e69bd1117997670e6d13a52
Bernadette Davis <bsd231@nyu.edu>
2019-10-27 21:08:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ class App < Sinatra::Base
     erb :user_input
   end
   post '/piglatinize' do
-    #PigLatinizer.new()
+    latinize = PigLatinizer.new()
+    latinize.piglatinize(params[:user_phrase])
     params[:user_phrase]
   end
 end
