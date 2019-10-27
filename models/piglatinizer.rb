cbc49ca30b003c15157dad7f495c2039191c6efc
Bernadette Davis <bsd231@nyu.edu>
2019-10-27 21:00:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,43 +6,38 @@ class PigLatinizer
   def piglatinize(sentence) #words
     array = split(sentence)
     resultArray = []
-    #array = sentence.split(" ")
-
 
     array.each {|word|
-      #puts word
-
-
-    string = ""
-    foundVowel = false
-    word.chars.each {|letter|
-      if !letter.match(/[aeiouAEIOU]/)
-        string = string + letter
-      else
-        foundVowel = true
-        break
-      end
-    }
-    if foundVowel && string.length == 0
+      string = ""
+      foundVowel = false
       word.chars.each {|letter|
-        if letter.match(/[aeiouAEIOU]/)
+        if !letter.match(/[aeiouAEIOU]/)
           string = string + letter
+        else
+          foundVowel = true
+          break
         end
       }
-    end
+      if foundVowel && string.length == 0
+        word.chars.each {|letter|
+          if letter.match(/[aeiouAEIOU]/)
+            string = string + letter
+          end
+        }
+      end
 
-    first = word[0..string.length-1]
-    following = word[string.length..-1]
+      first = word[0..string.length-1]
+      following = word[string.length..-1]
 
-    if first.match(/[aeiouAEIOU]/)
-      word = first + following + "way"
-    else
-      word = following + first + "ay"
-    end
-    resultArray << word
+      if first.match(/[aeiouAEIOU]/)
+        word = first + following + "way"
+      else
+        word = following + first + "ay"
+      end
+      resultArray << word
 
     }
-    #puts resultArray
+
     sentence = resultArray.join(" ")
     sentence
   end
